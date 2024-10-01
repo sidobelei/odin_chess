cb2d6274441f474e1b6414e265db3adff1af5510
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-10-01 11:41:58 -0700

    Add ChessPiece class

---
+++ lib/chess_piece.rb
@@ -0,0 +1,24 @@
+class ChessPiece 
+  attr_accessor :color, :name, :position
+
+  def initialize(color, name, position)
+    color = color.downcase
+    if color == "red" || color == "white"
+      @color = color
+    else
+      raise ArgumentError, "Invalid color input"
+    end
+
+    if color == "red"
+      @name ="\e[1m\e[31m#{name}\e[0m"
+    elsif color == "white"
+      @name = "\e[1m\e[37m#{name}\e[0m"
+    end
+
+    if position.length == 2 && Array(0..7).include?(position[0]) && Array(0..7).include?(position[1])
+      @position = position
+    else
+      raise ArgumentError, "Invalid coordinates input"
+    end
+  end
+end
