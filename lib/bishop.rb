eb2b88cd44e724e6b7f9fa6e4b6a7b1f42b9aff6
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-10-04 12:23:12 -0700

    Add out_of_bounds? method to Bishop class

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -4,4 +4,8 @@ class Bishop < ChessPiece
   def initialize(color, position)
     super(color, 'B', 'bishop', position)
   end
+
+  def out_of_bounds?(pos)
+    pos[0] > 7 || pos[0] < 0 || pos[1] > 7 || pos[1] < 0
+  end
 end
