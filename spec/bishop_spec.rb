a9c0b5962d4796a1338a424f5038b193c3050d68
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-10-04 10:50:03 -0700

    Add test for type attribute and remove method call
    Added another attribute to the ChessPiece
    superclass and added a test to reflect that. Also
    removed method call to update_possible_moves that
    was previously in the initialize method of the
    ChessPiece class.

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/bishop'
 describe Bishop do
   describe '#initialize' do
     subject(:bishop) { described_class.new('red', [7, 2])}
-    
+
     context 'when a Bishop object is initialized' do
       it 'assigns a color attribute' do
         expect(bishop.color).to eq('red')    
@@ -13,13 +13,12 @@ describe Bishop do
         expect(bishop.name).to eq("\e[1m\e[31mB\e[0m")
       end
 
-      it 'assigns a position attribute' do
-        expect(bishop.position).to eq([7, 2])
+      it 'assigns a type attribute' do
+        expect(bishop.type).to eq("bishop")
       end
 
-      it 'calls update_possible_moves' do
-        allow_any_instance_of(Bishop).to receive(:update_possible_moves)
-        Bishop.new('red', [7,2])
+      it 'assigns a position attribute' do
+        expect(bishop.position).to eq([7, 2])
       end
     end
   end
