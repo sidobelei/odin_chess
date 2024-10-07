af8f6491f2d1fcdc5113102e03cff1552af2c78a
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-10-07 12:17:14 -0700

    Add tests for opponent_piece? in ChessPiece class

--- spec/chess_piece_spec.rb
+++ spec/chess_piece_spec.rb
@@ -144,4 +144,80 @@ describe ChessPiece do
       end
     end
   end
+
+  describe '#opponent_piece?' do
+    subject(:king1) { described_class.new('red', 'K', 'king', [0, 0]) }
+    subject(:king2) { described_class.new('white', 'K', 'king', [7,7]) }
+    subject(:rook1) { described_class.new('red', 'R', 'rook', [0, 1]) }
+    subject(:rook2) { described_class.new('red', 'R', 'rook', [7, 4]) }
+    subject(:rook3) { described_class.new('white', 'R', 'rook', [0, 1]) }
+    subject(:rook4) { described_class.new('white', 'R', 'rook', [7, 4]) }
+    subject(:bishop1) { described_class.new('red', 'B', 'bishop', [0, 4]) }
+    subject(:bishop2) { described_class.new('red', 'B', 'bishop', [6, 7]) }
+    subject(:bishop3) { described_class.new('white', 'B', 'bishop', [0, 4]) }
+    subject(:bishop4) { described_class.new('white', 'B', 'bishop', [6, 7]) }
+    subject(:pawn1) { described_class.new('red', 'P', 'pawn', [3, 0]) }
+    subject(:pawn2) { described_class.new('red', 'P', 'pawn', [3, 7]) }
+    subject(:pawn3) { described_class.new('red', 'P', 'pawn', [7, 4]) }
+    subject(:pawn4) { described_class.new('white', 'P', 'pawn', [3, 0]) }
+    subject(:pawn5) { described_class.new('white', 'P', 'pawn', [3, 7]) }
+    subject(:pawn6) { described_class.new('white', 'P', 'pawn', [7, 4]) }
+    subject(:knight1) { described_class.new('red', 'N', 'knight', [7, 0]) }
+    subject(:knight2) { described_class.new('white', 'N', 'knight', [7, 0]) }
+    subject(:queen1) { described_class.new('red', 'Q', 'queen', [3, 4]) }
+    
+    let(:board_self) { [
+      king1,
+      king2,
+      rook1,
+      rook2,
+      bishop1,
+      bishop2,
+      pawn1,
+      pawn2,
+      pawn3,
+      knight1,
+      queen1
+    ] }
+
+    let(:board_opponent) { [
+      king1,
+      king2,
+      rook3,
+      rook4,
+      bishop3,
+      bishop4,
+      pawn4,
+      pawn5,
+      pawn6,
+      knight2,
+      queen1
+    ] }
+
+    context 'when the square on the board is occupied by a piece from your side' do
+      it 'returns false' do
+        expect(queen1.opponent_piece?(board_self, [0, 1])).to eq(false)
+        expect(queen1.opponent_piece?(board_self, [7, 4])).to eq(false)
+        expect(queen1.opponent_piece?(board_self, [0, 4])).to eq(false)
+        expect(queen1.opponent_piece?(board_self, [6, 7])).to eq(false)
+        expect(queen1.opponent_piece?(board_self, [3, 0])).to eq(false)
+        expect(queen1.opponent_piece?(board_self, [3, 7])).to eq(false)
+        expect(queen1.opponent_piece?(board_self, [7, 4])).to eq(false)
+        expect(queen1.opponent_piece?(board_self, [7, 0])).to eq(false)
+      end
+    end
+
+    context 'when the square on the board is occupied by a piece from the opponent' do
+      it 'returns true' do
+        expect(queen1.opponent_piece?(board_opponent, [0, 1])).to eq(true)
+        expect(queen1.opponent_piece?(board_opponent, [7, 4])).to eq(true)
+        expect(queen1.opponent_piece?(board_opponent, [0, 4])).to eq(true)
+        expect(queen1.opponent_piece?(board_opponent, [6, 7])).to eq(true)
+        expect(queen1.opponent_piece?(board_opponent, [3, 0])).to eq(true)
+        expect(queen1.opponent_piece?(board_opponent, [3, 7])).to eq(true)
+        expect(queen1.opponent_piece?(board_opponent, [7, 4])).to eq(true)
+        expect(queen1.opponent_piece?(board_opponent, [7, 0])).to eq(true)
+      end
+    end
+  end
 end
