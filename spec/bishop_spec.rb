db330534c7d2a664e1104ff2bbcf1b1919d87b91
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-10-07 15:53:37 -0700

    Add tests for update_possible_moves in Bishop

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -22,4 +22,82 @@ describe Bishop do
       end
     end
   end
+
+  describe '#update_possible_moves' do
+    let(:bishop) { described_class.new('white', [3, 3]) }
+    let(:king1) { double("King", color: 'red', name: 'K', type: 'king', position: [0, 1]) }
+    let(:rook1) { double("Rook", color: 'red', name: 'R', type: 'rook', position: [0, 2]) }
+    let(:rook2) { double("Rook", color: 'red', name: 'R', type: 'rook', position: [0, 3]) }
+    let(:pawn1) { double("Pawn", color: 'red', name: 'P', type: 'pawn', position: [1, 0]) }
+    let(:pawn2) { double("Pawn", color: 'red', name: 'P', type: 'pawn', position: [1, 1]) }
+    let(:pawn3) { double("Pawn", color: 'red', name: 'P', type: 'pawn', position: [1, 5]) }
+    let(:knight) { double("Knight", color: 'red', name: 'N', type: 'knight', position: [4, 2]) }
+    let(:king2) { double("King", color: 'white', name: 'K', type: 'king', position: [5, 5]) }
+    let(:board_clear) { [
+      bishop,
+      king1,
+      rook1,
+      rook2,
+      pawn1,
+      king2
+    ] }
+    let(:board_obstructed) { [
+      bishop,
+      king1,
+      rook1,
+      rook2,
+      pawn1,
+      pawn2,
+      pawn3,
+      knight,
+      king2
+    ] }
+    
+    context 'when there are no pieces in the path of the bishop' do
+      before do
+        allow(bishop).to receive(:out_of_bounds?).and_return(false, false, false, true, false, false, false, true, false, false, false, true, false, false, false, false, true)
+        allow(bishop).to receive(:king_or_same_color?).and_return(false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false)
+        allow(bishop).to receive(:opponent_piece?).and_return(false, false, false, false, false, false, false, false, false, false, false, false, false)
+      end
+
+      it 'possible_moves has all moves' do
+        bishop.update_possible_moves(board_clear)
+        expect(bishop.possible_moves).to eq([
+          [2, 2],
+          [1, 1],
+          [0, 0],
+          [2, 4],
+          [1, 5],
+          [0, 6],
+          [4, 2],
+          [5, 1],
+          [6, 0],
+          [4, 4],
+          [5, 5],
+          [6, 6],
+          [7, 7]
+        ])
+      end
+    end
+
+    context 'when there are chess pieces in the path of the bishop' do
+      before do
+        allow(bishop).to receive(:out_of_bounds?).and_return(false, false, false, false, false, false, false)
+        allow(bishop).to receive(:king_or_same_color?).and_return(false, false, false, false, false, false, true)
+        allow(bishop).to receive(:opponent_piece?).and_return(false, true, false, true, true, false, false)
+      end
+
+      it 'possible_moves has a limited set of moves' do
+        bishop.update_possible_moves(board_obstructed)
+        expect(bishop.possible_moves).to eq([
+          [2, 2],
+          [1, 1],
+          [2, 4],
+          [1, 5],
+          [4, 2],
+          [4, 4]
+        ])
+      end
+    end
+  end
 end
