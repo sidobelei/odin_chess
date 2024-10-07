5111582e7582a4ad17a018c2d5eb87d448943507
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-10-07 15:54:28 -0700

    Add update_possible_moves method to Bishop class

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -4,4 +4,29 @@ class Bishop < ChessPiece
   def initialize(color, position)
     super(color, 'B', 'bishop', position)
   end
+
+  def update_possible_moves(board)
+    new_moves = []
+    movement = [
+      [-1, -1],
+      [-1, 1],
+      [1, -1],
+      [1, 1]
+    ] 
+    movement.each do |move|
+      temp = [
+        position[0] + move[0], 
+        position[1] + move[1]
+      ]
+      until out_of_bounds?(temp) || king_or_same_color?(board, temp)
+        new_moves << temp
+        break if opponent_piece?(board, temp)
+        temp = [
+          temp[0] + move[0],
+          temp[1] + move[1]
+        ]
+      end
+    end
+    @possible_moves = new_moves
+  end
 end
