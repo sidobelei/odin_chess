0071f46c16e49ba1c7ef451537b9644969b9b3bc
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-10-10 10:18:35 -0700

    Add update_possible_moves method to Knight class

--- lib/knight.rb
+++ lib/knight.rb
@@ -3,5 +3,29 @@ require_relative 'chess_piece'
 class Knight < ChessPiece
   def initialize(color, position)
     super(color, 'N', 'knight', position)
-  end  
+  end
+  
+  def update_possible_moves(board)
+    new_moves = []
+    movement = [
+      [-2, -1],
+      [-2, 1],
+      [-1, 2],
+      [1, 2],
+      [2, 1],
+      [2, -1],
+      [1, -2],
+      [-1, -2]
+    ]
+    movement.each do |move|
+      temp = [
+        position[0] + move[0], 
+        position[1] + move[1]
+      ]
+      unless out_of_bounds?(temp) || king_or_same_color?(board, temp)
+        new_moves << temp
+      end
+    end
+    @possible_moves = new_moves
+  end
 end
