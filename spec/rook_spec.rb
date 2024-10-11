790398fb72cdad3f1f59518f1e3c418c514a2bc6
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-10-11 16:18:13 -0700

    Edit indentation on Rook class file

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -24,30 +24,30 @@ describe Rook do
         expect(rook.moved).to eq(false)
       end
     end
-
-    describe '#update_position' do
-      context 'when the Rook was not moved and is called to be moved' do
-        subject(:rook_unmoved) { described_class.new('white', [7, 0]) }
-        
-        it 'updates the current position and changes the moved attribute to true' do
-          rook_unmoved.update_position([5, 0])
-          expect(rook_unmoved.position).to eq([5, 0])
-          expect(rook_unmoved.moved).to eq(true)
-        end
-      end
-
-      context 'when the Rook was moved previously and is called to be moved again' do
-        subject(:rook_moved) { described_class.new('red', [0, 7]) }
-        
-        it 'updates the current position and moved attribute stays true' do
-          expect(rook_moved.moved).to eq(false)
-          rook_moved.update_position([0, 6])
-          expect(rook_moved.moved).to eq(true)
-          rook_moved.update_position([4, 6])
-          expect(rook_moved.position).to eq([4, 6])
-          expect(rook_moved.moved).to eq(true)
-        end
+  end
+  
+  describe '#update_position' do
+    context 'when the Rook was not moved and is called to be moved' do
+      subject(:rook_unmoved) { described_class.new('white', [7, 0]) }
+      
+      it 'updates the current position and changes the moved attribute to true' do
+        rook_unmoved.update_position([5, 0])
+        expect(rook_unmoved.position).to eq([5, 0])
+        expect(rook_unmoved.moved).to eq(true)
       end
     end
-  end  
+
+    context 'when the Rook was moved previously and is called to be moved again' do
+      subject(:rook_moved) { described_class.new('red', [0, 7]) }
+      
+      it 'updates the current position and moved attribute stays true' do
+        expect(rook_moved.moved).to eq(false)
+        rook_moved.update_position([0, 6])
+        expect(rook_moved.moved).to eq(true)
+        rook_moved.update_position([4, 6])
+        expect(rook_moved.position).to eq([4, 6])
+        expect(rook_moved.moved).to eq(true)
+      end
+    end
+  end
 end
