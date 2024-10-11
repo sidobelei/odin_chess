cb8685c56bd729b7222965ee1a17d728b7d6f998
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-10-11 15:24:42 -0700

    Add initialize method to Rook class

--- lib/rook.rb
+++ lib/rook.rb
@@ -1,5 +1,10 @@
 require_relative 'chess_piece'
 
-class Rook
-  
+class Rook < ChessPiece
+  attr_accessor :moved
+
+  def initialize(color, position) 
+    super(color, 'R', 'rook', position)
+    @moved = false
+  end
 end
