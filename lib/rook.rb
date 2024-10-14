dc79abc5fe10955ede3c8339b9f4d17cab3392d4
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-10-14 16:09:45 -0700

    Add update_possible_moves method in Rook class

--- lib/rook.rb
+++ lib/rook.rb
@@ -14,4 +14,29 @@ class Rook < ChessPiece
       @moved = true
     end
   end
+
+  def update_possible_moves(board)
+    new_moves = []
+    movement = [
+      [-1, 0],
+      [0, 1],
+      [1, 0],
+      [0, -1]
+    ] 
+    movement.each do |move|
+      temp = [
+        position[0] + move[0], 
+        position[1] + move[1]
+      ]
+      until out_of_bounds?(temp) || king_or_same_color?(board, temp)
+        new_moves << temp
+        break if opponent_piece?(board, temp)
+        temp = [
+          temp[0] + move[0],
+          temp[1] + move[1]
+        ]
+      end
+    end
+    @possible_moves = new_moves
+  end
 end
