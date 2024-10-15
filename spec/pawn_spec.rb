496cf605e9556abbb56cf40580c05e55629b3e7c
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-10-14 18:09:28 -0700

    Refactor tests for initialize method in Pawn class

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -5,27 +5,27 @@ describe Pawn do
     subject(:pawn) { described_class.new('white', [4, 4]) }
     
     context 'when a Pawn object is initialized' do
-      xit 'assigns a color attribute' do
+      it 'assigns a color attribute' do
         expect(pawn.color).to eq('white')
       end
       
-      xit 'assigns a name attribute' do
-        expect(pawn.name).to eq('\e[1m\e[37mP\e[0m')
+      it 'assigns a name attribute' do
+        expect(pawn.name).to eq("\e[1m\e[37mP\e[0m")
       end
       
-      xit 'assigns a type attribute' do
+      it 'assigns a type attribute' do
         expect(pawn.type).to eq('pawn')
       end
       
-      xit 'assigns a position attribute' do
+      it 'assigns a position attribute' do
         expect(pawn.position).to eq([4, 4])
       end
       
-      xit 'assigns false to the moved attribute' do
+      it 'assigns false to the moved attribute' do
         expect(pawn.moved).to eq(false)
       end
       
-      xit 'assigns false to the promoted attribute' do
+      it 'assigns false to the promoted attribute' do
         expect(pawn.promoted).to eq(false)
       end
     end
