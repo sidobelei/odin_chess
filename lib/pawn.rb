806c7daff0d1a101eeb805102da76ad26287957c
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-10-15 14:28:24 -0700

    Override update_position method in Pawn class

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -1,11 +1,26 @@
 require_relative 'chess_piece'
 
 class Pawn < ChessPiece
-attr_accessor :moved, :promoted
+attr_accessor :moved, :promoted, :opposite_row
 
   def initialize(color, position)
     super(color, 'P', 'pawn', position)
     @moved = false
     @promoted = false
+    if color == 'red'
+      @opposite_row = 7
+    elsif color == 'white'
+      @opposite_row = 0
+    end
+    
+  end
+
+  def update_position(new_position)
+    if new_position[0] == opposite_row
+      @position = nil
+      @promoted = true
+    else
+      @position = new_position
+    end
   end
 end
