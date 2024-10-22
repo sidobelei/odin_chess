41d6c015bfc235e1b89a19190b4d60ed2ffb53b7
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-10-22 15:49:56 -0700

    Add update_possible_moves method in Pawn class

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -1,7 +1,7 @@
 require_relative 'chess_piece'
 
 class Pawn < ChessPiece
-attr_accessor :moved, :promoted, :opposite_row
+attr_accessor :moved, :promoted, :opposite_row, :direction
 
   def initialize(color, position)
     super(color, 'P', 'pawn', position)
@@ -9,8 +9,10 @@ attr_accessor :moved, :promoted, :opposite_row
     @promoted = false
     if color == 'red'
       @opposite_row = 7
+      @direction = 1
     elsif color == 'white'
       @opposite_row = 0
+      @direction = -1
     end
     
   end
@@ -26,5 +28,32 @@ attr_accessor :moved, :promoted, :opposite_row
     else
       @position = new_position
     end
+  end
+
+  def update_possible_moves(board)
+    new_moves = []
+    movement = [
+      [direction, -1, 'attack'],
+      [direction, 0, 'movement'],
+      [direction, 1, 'attack'],
+    ]
+    movement.each_with_index do |move, index|
+      temp = [
+        position[0] + move[0], 
+        position[1] + move[1]
+      ]
+      if opponent_piece?(board, temp) && move[2] == 'attack'
+        new_moves << temp
+      elsif out_of_bounds?(temp) == false && king_or_same_color?(board, temp) == false && opponent_piece?(board, temp) == false && move[2] == 'movement'
+        new_moves << temp
+        temp = [temp[0] + direction, temp[1]]
+        if moved == false && opponent_piece?(board, temp) == false && king_or_same_color?(board, temp) == false
+          new_moves << temp
+        end
+      else
+        next 
+      end
+      @possible_moves = new_moves
+    end 
   end
 end
