3067b2cef992f82b8f2049677e3b913b18f3ce92
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-10-25 15:03:04 -0700

    Add initialize method to King class

--- lib/king.rb
+++ lib/king.rb
@@ -1,5 +1,10 @@
 require_relative 'chess_piece'
 
 class King < ChessPiece
-
+  attr_accessor :moved
+  
+  def initialize(color, position) 
+    super(color, 'K', 'king', position)
+    @moved = 0
+  end
 end
