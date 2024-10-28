3cc825702b35120af946be6553e1f62adb1fa166
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-10-28 13:06:11 -0700

    Overwrite update_position method in King class

--- lib/king.rb
+++ lib/king.rb
@@ -2,9 +2,14 @@ require_relative 'chess_piece'
 
 class King < ChessPiece
   attr_accessor :moved
-  
+
   def initialize(color, position) 
     super(color, 'K', 'king', position)
     @moved = 0
+  end
+
+  def update_position(new_position) 
+    @position = new_position
+    @moved += 1
   end
 end
