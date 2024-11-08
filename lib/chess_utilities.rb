51099f9e342d80ea784e6b4d476507c43f3a59a2
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-11-08 15:21:20 -0800

    Move out_of_bounds? and in_check? to a module

--- lib/chess_utilities.rb
+++ lib/chess_utilities.rb
@@ -1,3 +1,19 @@
 module ChessUtilites
-  
+  def out_of_bounds?(pos)
+    pos[0] > 7 || pos[0] < 0 || pos[1] > 7 || pos[1] < 0
+  end
+
+  def in_check?(board, pos)
+    board.each do |piece|
+      if piece == self
+        next
+      end
+      piece.possible_moves.each do |move|
+        if move == pos && piece.color != @color
+          return true 
+        end
+      end
+    end
+    return false
+  end
 end
