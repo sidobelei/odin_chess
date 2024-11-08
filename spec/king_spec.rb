e09980125fd2ee5e7110be21534c30ec5d8cc64b
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-11-08 14:44:43 -0800

    Add tests for update_possible_moves in King class

--- spec/king_spec.rb
+++ spec/king_spec.rb
@@ -1125,4 +1125,271 @@ describe King do
       end
    end
   end
+
+  describe '#update_possible_moves' do
+    subject(:king_white) { described_class.new('white', [7, 4]) }
+    subject(:king_red) { described_class.new('red', [0, 4]) }
+    
+    let(:pawn_white_1) { double('Pawn', color: 'white', type: 'pawn', position: [2, 3], possible_moves: [[1, 3]]) }
+    let(:knight_white_1) { double('Knight', color: 'white', type: 'knight', position: [2, 4], possible_moves: [[0, 3], [0, 5]]) }
+    let(:pawn_white_2) { double('Pawn', color: 'white', type: 'pawn', position: [2, 5], possible_moves: [[1, 5]]) }
+    let(:bishop_white_1) { double('Bishop', color: 'white', type: 'bishop', position: [3, 3], possible_moves: [[2, 2], [4, 2]]) }
+    let(:bishop_white_2) { double('Bishop', color: 'white', type: 'bishop', position: [3, 5], possible_moves: [[2, 6], [4, 6]]) }
+    let(:rook_white_1) { double('Rook', color: 'white', type: 'rook', position: [4, 3], possible_moves: [[4, 2], [5, 3]]) }
+    let(:knight_white_2) { double('Knight', color: 'white', type: 'knight', position: [4, 4], possible_moves: [[6, 3], [6, 5]]) }
+    let(:rook_white_2) { double('Rook', color: 'white', type: 'rook', position: [4, 5], possible_moves: [[4, 6], [5, 5]]) }
+
+    let(:queen_white) { double('Queen', color: 'white', type: 'queen', position: [1, 4], possible_moves:[[2, 3], [2, 4], [2, 5]]) }
+    let(:rook_white_3) { double('Rook', color: 'white', type: 'rook', position: [3, 6], possible_moves:[[3, 5]]) }
+    let(:knight_white_3) { double('Knight', color: 'white', type: 'knight', position: [5, 2], possible_moves:[[3, 3]]) }
+    let(:bishop_white_3) { double('Bishop', color: 'white', type: 'bishop', position: [5, 3], possible_moves:[[4, 4]]) }
+    let(:pawn_white_3) { double('Pawn', color: 'white', type: 'pawn', position: [5, 4], possible_moves:[[4, 3], [4, 5]]) }
+
+    let(:queen_red) { double('Queen', color: 'red', type: 'queen', position:[3, 3], possible_moves: []) }
+    let(:rook_red) { double('Rook', color: 'red', type: 'rook', position:[3, 4], possible_moves: []) }
+    let(:knight_red) { double('Knight', color: 'red', type: 'knight', position:[3, 5], possible_moves: []) }
+    let(:bishop_red_1) { double('Bishop', color: 'red', type: 'bishop', position:[4, 3], possible_moves: []) }
+    let(:bishop_red_2) { double('Bishop', color: 'red', type: 'bishop', position:[4, 5], possible_moves: []) }
+    let(:pawn_red_1) { double('Pawn', color: 'red', type: 'pawn', position:[5, 3], possible_moves: []) }
+    let(:pawn_red_2) { double('Pawn', color: 'red', type: 'pawn', position:[5, 4], possible_moves: []) }
+    let(:pawn_red_3) { double('Pawn', color: 'red', type: 'pawn', position:[5, 5], possible_moves: []) }
+
+    let(:unmoved_rook_1) { double('Rook', color: 'red', type: 'rook', position: [0, 0], moved: 0, possible_moves: []) }
+    let(:unmoved_rook_2) { double('Rook', color: 'red', type: 'rook', position: [0, 7], moved: 0, possible_moves: []) }
+
+    let(:board_empty) { [
+      king_white,
+    ] }
+    let(:board_opposing_boxed) { [
+      king_red,
+      pawn_white_1,
+      knight_white_1,
+      pawn_white_2,
+      bishop_white_1,
+      bishop_white_2,
+      rook_white_1,
+      knight_white_2,
+      rook_white_2
+    ] }
+    let(:board_opposing_check) { [
+      king_red,
+      queen_white,
+      rook_white_3,
+      knight_white_3,
+      bishop_white_3,
+      pawn_white_3
+    ] }
+    let(:board_opposing_boxed_check) { [
+      king_red,
+      queen_white,
+      rook_white_3,
+      knight_white_3,
+      rook_white_1,
+      knight_white_2,
+      rook_white_2
+    ] }
+    let(:board_same_boxed) { [
+      king_red,
+      queen_red,
+      rook_red,
+      knight_red,
+      bishop_red_1,
+      bishop_red_2,
+      pawn_red_1,
+      pawn_red_2,
+      pawn_red_3
+    ] }
+    let(:board_mixed) { [
+      king_red,
+      queen_white,
+      rook_white_3,
+      bishop_red_1,
+      bishop_red_2,
+      bishop_white_3
+    ] }
+    let(:board_castling) { [
+      king_red,
+      unmoved_rook_1,
+      unmoved_rook_2
+    ] }
+
+    context 'when there no other chess pieces and the King is not in check' do
+      context 'when the King is in the middle of the board' do
+        it 'generates the correct set of moves' do
+          king_white.update_position([4, 4])
+          king_white.update_possible_moves(board_empty)
+          expect(king_white.possible_moves).to eq([
+            [3, 3],
+            [3, 4],
+            [3, 5],
+            [4, 5],
+            [5, 5],
+            [5, 4],
+            [5, 3],
+            [4, 3]
+          ])
+        end
+      end
+
+      context 'when the King is at the top of the board' do
+        it 'generates the correct set of moves' do
+          king_white.update_position([0, 4])
+          king_white.update_possible_moves(board_empty)
+          expect(king_white.possible_moves).to eq([
+            [0, 5],
+            [1, 5],
+            [1, 4],
+            [1, 3],
+            [0, 3]
+          ])
+        end
+      end
+
+      context 'when the King is at the bottom of the board' do
+        it 'generates the correct set of moves' do
+          king_white.update_possible_moves(board_empty)
+          expect(king_white.possible_moves).to eq([
+            [6, 3],
+            [6, 4],
+            [6, 5],
+            [7, 5],
+            [7, 3]
+          ])
+        end
+      end
+      
+      context 'when the King is on the left edge of the board' do
+        it 'generates the correct set of moves' do
+          king_white.update_position([3, 0])
+          king_white.update_possible_moves(board_empty)
+          expect(king_white.possible_moves).to eq([
+            [2, 0],
+            [2, 1],
+            [3, 1],
+            [4, 1],
+            [4, 0]
+          ])
+        end
+      end
+
+      context 'when  the King is on the right edage of the board' do
+        it 'generates the correct set of moves' do
+          king_white.update_position([4, 7])
+          king_white.update_possible_moves(board_empty)
+          expect(king_white.possible_moves).to eq([
+            [3, 6],
+            [3, 7],
+            [5, 7],
+            [5, 6],
+            [4, 6]
+          ])
+        end
+      end
+
+      context 'when the King is on the top left corner of the board' do
+        it 'generates the correct set of moves' do
+          king_white.update_position([0, 0])
+          king_white.update_possible_moves(board_empty)
+          expect(king_white.possible_moves).to eq([
+            [0, 1],
+            [1, 1],
+            [1, 0]
+          ])
+        end
+      end
+
+      context 'when the King is on the top right corner of the board' do
+        it 'generates the correct set of moves' do
+          king_white.update_position([0, 7])
+          king_white.update_possible_moves(board_empty)
+          expect(king_white.possible_moves).to eq([
+            [1, 7],
+            [1, 6],
+            [0, 6]
+          ])
+        end
+      end
+
+      context 'when the King is on the bottom left corner of the board' do
+        it 'generates the correct set of moves' do
+          king_white.update_position([7, 0])
+          king_white.update_possible_moves(board_empty)
+          expect(king_white.possible_moves).to eq([
+            [6, 0],
+            [6, 1],
+            [7, 1]
+          ])
+        end
+      end
+
+      context 'when the King is on the bottom right corner of the board' do
+        it 'generates the correct set of moves' do
+          king_white.update_position([7, 7])
+          king_white.update_possible_moves(board_empty)
+          expect(king_white.possible_moves).to eq([
+            [6, 6],
+            [6, 7],
+            [7, 6]
+          ])
+        end
+      end
+    end
+
+    context 'when there are opposing chess pieces around the King' do
+      context 'when the King is boxed in but not in check' do
+        it 'does not generate any moves' do
+          king_white.update_position([3, 4])
+          king_white.update_possible_moves(board_opposing_boxed)
+          expect(king_white.possible_moves).to eq([])
+        end
+      end
+
+      context 'when all the potential moves of the King puts it in check' do
+        it 'does not generate any moves' do
+          king_red.update_position([3, 4])
+          king_red.update_possible_moves(board_opposing_check)
+          expect(king_red.possible_moves).to eq([])
+        end
+      end
+
+      context 'when the mix of opposing pieces block or place the King in check' do
+        it 'does not generate any moves' do
+          king_red.update_position([3, 4])
+          king_red.update_possible_moves(board_opposing_boxed_check)
+          expect(king_red.possible_moves).to eq([])
+        end
+      end
+    end
+
+    context 'when there are chess pieces of ths same color' do
+      it 'does not generate any moves' do
+        king_red.update_position([4, 4])
+        king_red.update_possible_moves(board_same_boxed)
+        expect(king_red.possible_moves).to eq([])
+      end
+    end
+
+    context 'when there is a mix of different chess pieces around the King' do
+      it 'generates a subset of all possible moves' do
+        king_red.update_position([3, 4])
+        king_red.update_possible_moves(board_mixed)
+        expect(king_red.possible_moves).to eq([[3, 3]])
+      end
+    end
+
+    context 'when there is an opportunity for the King to castle and there are no chess pieces obstructing it' do
+      it 'generates all the possible moves' do
+        king_red.update_possible_moves(board_castling)
+        expect(king_red.possible_moves).to eq([
+          [0, 5],
+          [1, 5],
+          [1, 4],
+          [1, 3],
+          [0, 3],
+          '[0-0-0]',
+          '[0-0]'
+        ])
+      end
+    end
+  end
 end
