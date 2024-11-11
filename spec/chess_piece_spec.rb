00b03ddf90d495448bc2e9178c94d3c15176ac9d
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-11-11 11:57:37 -0800

    Remove out_of_bounds tests from chess_piece spec

--- spec/chess_piece_spec.rb
+++ spec/chess_piece_spec.rb
@@ -62,31 +62,6 @@ describe ChessPiece do
     end
   end
 
-  describe '#out_of_bounds?' do
-    subject(:in_bounds) { described_class.new('white', 'Q', 'queen', [7, 5] )}
-    subject(:out_bounds) { described_class.new('red', 'R', 'rook', [0, 2]) }
-    
-    context 'when given a position that is not out of bounds of the board' do
-      it 'returns false' do
-        expect(in_bounds.out_of_bounds?([7, 0])).to eq(false)
-        expect(in_bounds.out_of_bounds?([7, 7])).to eq(false)
-        expect(in_bounds.out_of_bounds?([0, 7])).to eq(false)
-        expect(in_bounds.out_of_bounds?([0, 0])).to eq(false)
-        expect(in_bounds.out_of_bounds?([4, 3])).to eq(false)
-      end
-    end
-
-    context 'when given a position that is out of bounds' do
-      it 'returns true' do
-        expect(out_bounds.out_of_bounds?([-1, 7])).to eq(true)
-        expect(out_bounds.out_of_bounds?([-1, 0])).to eq(true)
-        expect(out_bounds.out_of_bounds?([8, 0])).to eq(true)
-        expect(out_bounds.out_of_bounds?([8, 7])).to eq(true)
-        expect(out_bounds.out_of_bounds?([-100, 800])).to eq(true)
-      end
-    end
-  end
-
   describe '#king_or_same_color?' do
     subject(:king1) { described_class.new('red', 'K', 'king', [0, 1]) }
     subject(:king2) { described_class.new('red', 'K', 'king', [7, 6]) }
