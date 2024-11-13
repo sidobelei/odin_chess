4cf9ceb423ab91d82b022e8de1b29e9dd2081d93
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-11-12 16:05:00 -0800

    Add tests for to_s method in Board class

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -53,4 +53,78 @@ describe Board do
       end
     end
   end
+
+  describe 'to_s' do
+    subject(:board_empty) { described_class.new }
+    subject(:board_starting) { described_class.new }
+    subject(:board_in_progress) { described_class.new }
+
+    let(:red_rook_1) { double(name: "\e[1m\e[31mR\e[0m", position: [0, 0]) }
+    let(:red_knight) { double(name: "\e[1m\e[31mN\e[0m", position: [0, 1]) }
+    let(:red_rook_2) { double(name: "\e[1m\e[31mR\e[0m", position: [0, 5]) }
+    let(:red_king) { double(name: "\e[1m\e[31mK\e[0m", position: [0, 6]) }
+    let(:red_pawn_1) { double(name: "\e[1m\e[31mP\e[0m", position: [1, 2]) }
+    let(:red_pawn_2) { double(name: "\e[1m\e[31mP\e[0m", position: [1, 5]) }
+    let(:red_pawn_3) { double(name: "\e[1m\e[31mP\e[0m", position: [1, 6]) }
+    let(:red_pawn_4) { double(name: "\e[1m\e[31mP\e[0m", position: [1, 7]) }
+    let(:red_pawn_5) { double(name: "\e[1m\e[31mP\e[0m", position: [2, 0]) }
+    let(:red_pawn_6) { double(name: "\e[1m\e[31mP\e[0m", position: [2, 4]) }
+    let(:red_pawn_7) { double(name: "\e[1m\e[31mP\e[0m", position: [3, 1]) }
+    let(:white_knight_1) { double(name: "\e[1m\e[37mN\e[0m", position: [3, 4]) }
+    let(:white_knight_2) { double(name: "\e[1m\e[37mN\e[0m", position: [3, 6]) }
+    let(:red_bishop) { double(name: "\e[1m\e[31mB\e[0m", position: [4, 1]) }
+    let(:white_pawn_1) { double(name: "\e[1m\e[37mP\e[0m", position: [4, 2]) }
+    let(:white_pawn_2) { double(name: "\e[1m\e[37mP\e[0m", position: [5, 6]) }
+    let(:red_queen) { double(name: "\e[1m\e[31mQ\e[0m", position: [6, 0]) }
+    let(:white_bishop) { double(name: "\e[1m\e[37mB\e[0m", position: [6, 1]) }
+    let(:white_queen) { double(name: "\e[1m\e[37mQ\e[0m", position: [6, 4]) }
+    let(:white_pawn_3) { double(name: "\e[1m\e[37mP\e[0m", position: [6, 5]) }
+    let(:white_king) { double(name: "\e[1m\e[37mK\e[0m", position: [6, 6]) }
+    let(:white_pawn_4) { double(name: "\e[1m\e[37mP\e[0m", position: [6, 7]) }
+    let(:white_rook) { double(name: "\e[1m\e[37mR\e[0m", position: [7, 5]) }
+
+    context 'when the board is object is initialized' do
+      it 'returns the string representation of the starting board' do
+        expect(board_starting.to_s).to eq("   +---+---+---+---+---+---+---+---+\n8  | \e[1m\e[31mR\e[0m | \e[1m\e[31mN\e[0m | \e[1m\e[31mB\e[0m | \e[1m\e[31mQ\e[0m | \e[1m\e[31mK\e[0m | \e[1m\e[31mB\e[0m | \e[1m\e[31mN\e[0m | \e[1m\e[31mR\e[0m |\n   +---+---+---+---+---+---+---+---+\n7  | \e[1m\e[31mP\e[0m | \e[1m\e[31mP\e[0m | \e[1m\e[31mP\e[0m | \e[1m\e[31mP\e[0m | \e[1m\e[31mP\e[0m | \e[1m\e[31mP\e[0m | \e[1m\e[31mP\e[0m | \e[1m\e[31mP\e[0m |\n   +---+---+---+---+---+---+---+---+\n6  |   |   |   |   |   |   |   |   |\n   +---+---+---+---+---+---+---+---+\n5  |   |   |   |   |   |   |   |   |\n   +---+---+---+---+---+---+---+---+\n4  |   |   |   |   |   |   |   |   |\n   +---+---+---+---+---+---+---+---+\n3  |   |   |   |   |   |   |   |   |\n   +---+---+---+---+---+---+---+---+\n2  | \e[1m\e[37mP\e[0m | \e[1m\e[37mP\e[0m | \e[1m\e[37mP\e[0m | \e[1m\e[37mP\e[0m | \e[1m\e[37mP\e[0m | \e[1m\e[37mP\e[0m | \e[1m\e[37mP\e[0m | \e[1m\e[37mP\e[0m |\n   +---+---+---+---+---+---+---+---+\n1  | \e[1m\e[37mR\e[0m | \e[1m\e[37mN\e[0m | \e[1m\e[37mB\e[0m | \e[1m\e[37mQ\e[0m | \e[1m\e[37mK\e[0m | \e[1m\e[37mB\e[0m | \e[1m\e[37mN\e[0m | \e[1m\e[37mR\e[0m |\n   +---+---+---+---+---+---+---+---+\n     a   b   c   d   e   f   g   h\n\n")
+      end
+    end
+
+    context 'when the board is empty' do
+      it 'returns the string representation of an empty board' do
+        board_empty.display = []
+        expect(board_empty.to_s).to eq("   +---+---+---+---+---+---+---+---+\n8  |   |   |   |   |   |   |   |   |\n   +---+---+---+---+---+---+---+---+\n7  |   |   |   |   |   |   |   |   |\n   +---+---+---+---+---+---+---+---+\n6  |   |   |   |   |   |   |   |   |\n   +---+---+---+---+---+---+---+---+\n5  |   |   |   |   |   |   |   |   |\n   +---+---+---+---+---+---+---+---+\n4  |   |   |   |   |   |   |   |   |\n   +---+---+---+---+---+---+---+---+\n3  |   |   |   |   |   |   |   |   |\n   +---+---+---+---+---+---+---+---+\n2  |   |   |   |   |   |   |   |   |\n   +---+---+---+---+---+---+---+---+\n1  |   |   |   |   |   |   |   |   |\n   +---+---+---+---+---+---+---+---+\n     a   b   c   d   e   f   g   h\n\n")
+      end
+    end
+
+    context 'when the board has pieces moved' do
+      it 'returns the string representation of the board with the pieces moved' do
+        board_in_progress.display = [
+          red_rook_1,
+          red_knight,
+          red_rook_2,
+          red_king,
+          red_pawn_1,
+          red_pawn_2,
+          red_pawn_3,
+          red_pawn_4,
+          red_pawn_5,
+          red_pawn_6,
+          red_pawn_7,
+          white_knight_1,
+          white_knight_2,
+          red_bishop,
+          white_pawn_1,
+          white_pawn_2,
+          red_queen,
+          white_bishop,
+          white_queen,
+          white_pawn_3,
+          white_king,
+          white_pawn_4,
+          white_rook
+        ]
+        expect(board_in_progress.to_s).to eq("   +---+---+---+---+---+---+---+---+\n8  | \e[1m\e[31mR\e[0m | \e[1m\e[31mN\e[0m |   |   |   | \e[1m\e[31mR\e[0m | \e[1m\e[31mK\e[0m |   |\n   +---+---+---+---+---+---+---+---+\n7  |   |   | \e[1m\e[31mP\e[0m |   |   | \e[1m\e[31mP\e[0m | \e[1m\e[31mP\e[0m | \e[1m\e[31mP\e[0m |\n   +---+---+---+---+---+---+---+---+\n6  | \e[1m\e[31mP\e[0m |   |   |   | \e[1m\e[31mP\e[0m |   |   |   |\n   +---+---+---+---+---+---+---+---+\n5  |   | \e[1m\e[31mP\e[0m |   |   | \e[1m\e[37mN\e[0m |   | \e[1m\e[37mN\e[0m |   |\n   +---+---+---+---+---+---+---+---+\n4  |   | \e[1m\e[31mB\e[0m | \e[1m\e[37mP\e[0m |   |   |   |   |   |\n   +---+---+---+---+---+---+---+---+\n3  |   |   |   |   |   |   | \e[1m\e[37mP\e[0m |   |\n   +---+---+---+---+---+---+---+---+\n2  | \e[1m\e[31mQ\e[0m | \e[1m\e[37mB\e[0m |   |   | \e[1m\e[37mQ\e[0m | \e[1m\e[37mP\e[0m | \e[1m\e[37mK\e[0m | \e[1m\e[37mP\e[0m |\n   +---+---+---+---+---+---+---+---+\n1  |   |   |   |   |   | \e[1m\e[37mR\e[0m |   |   |\n   +---+---+---+---+---+---+---+---+\n     a   b   c   d   e   f   g   h\n\n")
+      end
+    end
+  end
 end
