4a735aa10954483bddb62cc9914654908b1f97b7
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-11-13 10:56:50 -0800

    Add test for initialize method in Game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,12 @@
 require_relative '../lib/game'
 
 describe Game do
-  describe '#' do
-    
+  describe '#initialize' do
+    context 'when a new Game object is initialized' do
+      it 'creates a new board object' do
+        expect(Board).to receive(:new).once
+        Game.new
+      end
+    end
   end
 end 
