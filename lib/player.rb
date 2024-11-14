1fb18cf532216f280346498417071e2ba1503b88
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-11-14 12:25:53 -0800

    Add valid_move? method to Player class

--- lib/player.rb
+++ lib/player.rb
@@ -5,4 +5,17 @@ class Player
     @my_pieces = pieces
     @inputs = []
   end
+
+  def valid_move?(pos, new_pos)  
+    my_pieces.each do |piece|
+      if piece.position == pos
+        piece.possible_moves.each do |move|
+          if new_pos == move
+            return true
+          end 
+        end
+      end
+    end
+    return false
+  end
 end
