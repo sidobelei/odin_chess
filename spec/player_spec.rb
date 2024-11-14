ce455a6473c66afa9ecdfd046fbddbf720dfb260
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-11-14 10:42:02 -0800

    Add test file for Player class

---
+++ spec/player_spec.rb
@@ -0,0 +1,39 @@
+require_relative '../lib/player'
+
+describe Player do
+  describe '#initialize' do
+    context 'when a Player object is initialized' do
+      let(:player_color) { 'white' }
+      let(:player_pieces) { [
+        {'type': 'king', 'color': player_color},
+        {'type': 'queen', 'color': player_color},
+        {'type': 'bishop', 'color': player_color},
+        {'type': 'bishop', 'color': player_color},
+        {'type': 'knight', 'color': player_color},
+        {'type': 'knight', 'color': player_color},
+        {'type': 'rook', 'color': player_color},
+        {'type': 'rook', 'color': player_color},
+        {'type': 'pawn', 'color': player_color},
+        {'type': 'pawn', 'color': player_color},
+        {'type': 'pawn', 'color': player_color},
+        {'type': 'pawn', 'color': player_color},
+        {'type': 'pawn', 'color': player_color},
+        {'type': 'pawn', 'color': player_color},
+        {'type': 'pawn', 'color': player_color},
+        {'type': 'pawn', 'color': player_color}
+      ] }
+      subject(:new_player) { described_class.new(player_color, player_pieces) }
+      it 'assigns a player attribute' do
+        expect(new_player.color).to eq(player_color)
+      end
+
+      it 'assigns an array to my_pieces attribute' do
+        expect(new_player.my_pieces).to eq(player_pieces)
+      end
+
+      it 'assigns an empty array to the inputs array' do
+        expect(new_player.inputs).to eq([])
+      end
+    end
+  end
+end
