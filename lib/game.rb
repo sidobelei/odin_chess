7cab0accfafe5b7f099a348c8ba346f463cb14e3
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-11-18 10:52:23 -0800

    Add player attributes to Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,12 @@
 require_relative 'board'
+require_relative 'player'
 
 class Game
+  attr_accessor :board, :player_1, :player_2
+
   def initialize
     @board = Board.new
+    @player_1 = Player.new('white', @board.display.select { |piece| piece.color == 'white'})
+    @player_2 = Player.new('red', @board.display.select { |piece| piece.color == 'red'})
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,10 +2,16 @@ require_relative '../lib/game'
 
 describe Game do
   describe '#initialize' do
+    subject(:game) { described_class.new }
+    
     context 'when a new Game object is initialized' do
       it 'creates a new board object' do
-        expect(Board).to receive(:new).once
-        Game.new
+        expect(game.board).to be_a(Board)
+      end
+
+      it 'creates two player objects' do
+        expect(game.player_1).to be_a(Player)
+        expect(game.player_2).to be_a(Player)
       end
     end
   end
