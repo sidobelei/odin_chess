7252d25ec05567d4a1cb96453a1229003a54e49f
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-11-21 14:07:02 -0800

    Refactor in_check? method to support all pieces

--- lib/chess_utilities.rb
+++ lib/chess_utilities.rb
@@ -4,13 +4,22 @@ module ChessUtilites
   end
 
   def in_check?(board, pos)
+    temp_board = []
     board.each do |piece|
       if piece == self
-        next
+        self_piece = piece.clone
+        self_piece.position = pos
+        temp_board << self_piece
+      else
+        temp_board << piece.clone
       end
+    end
+    
+    my_king = temp_board.find { |piece| piece.type == 'king' && piece.color == self.color }
+    temp_board.each do |piece|
       piece.possible_moves.each do |move|
-        if move == pos && piece.color != @color
-          return true 
+        if move == my_king.position && piece.color != my_king.color
+          return true
         end
       end
     end
