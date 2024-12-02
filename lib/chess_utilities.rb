e434c33eb6d325c0345c1f4d463442c68eb86518
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-12-02 15:25:22 -0800

    Refactor in_check? method in ChessUtilities module

--- lib/chess_utilities.rb
+++ lib/chess_utilities.rb
@@ -22,17 +22,55 @@ module ChessUtilites
         self_piece = piece.clone
         self_piece.position = pos
         temp_board << self_piece
+      elsif piece.position == pos && piece.color != @color
+        next
       else
         temp_board << piece.clone
       end
     end
-    
+  
     my_king = temp_board.find { |piece| piece.type == 'king' && piece.color == self.color }
-    temp_board.each do |piece|
-      piece.possible_moves.each do |move|
-        if move == my_king.position && piece.color != my_king.color
-          return true
+    my_king_pos = my_king.position
+    movement = [
+      {'position' => [-1, -1], 'continuous' => true, 'direction' => 'diagonal', 'pieces' => ['queen', 'bishop']},
+      {'position' => [-1, 0], 'continuous' => true, 'direction' => 'straight', 'pieces' => ['queen', 'rook']},
+      {'position' => [-1, 1], 'continuous' => true, 'direction' => 'diagonal', 'pieces' =>['queen', 'bishop']},
+      {'position' => [0, 1], 'continuous' => true, 'direction' => 'straight', 'pieces' => ['queen', 'rook']},
+      {'position' => [1, 1], 'continuous' => true, 'direction' => 'diagonal', 'pieces' => ['queen', 'bishop']},
+      {'position' => [1, 0], 'continuous' => true, 'direction' => 'straight', 'pieces' => ['queen', 'rook']},
+      {'position' => [1, -1], 'continuous' => true, 'direction' => 'diagonal', 'pieces' => ['queen', 'bishop']},
+      {'position' => [0, -1], 'continuous' => true, 'direction' => 'straight', 'pieces' => ['queen', 'rook']},
+      {'position' => [-2, -1], 'continuous' => true, 'direction' => 'l-shape', 'pieces' => ['knight']},
+      {'position' => [-2, 1], 'continuous' => true, 'direction' => 'l-shape', 'pieces' => ['knight']},
+      {'position' => [-1, 2], 'continuous' => true, 'direction' => 'l-shape', 'pieces' => ['knight']},
+      {'position' => [1, 2], 'continuous' => false, 'direction' => 'l-shape', 'pieces' => ['knight']},
+      {'position' => [2, 1], 'continuous' => false, 'direction' => 'l-shape', 'pieces' => ['knight']},
+      {'position' => [2, -1], 'continuous' => false, 'direction' => 'l-shape', 'pieces' => ['knight']},
+      {'position' => [1, -2], 'continuous' => false, 'direction' => 'l-shape', 'pieces' =>['knight']},
+      {'position' => [-1, -2], 'continuous' => false, 'direction' => 'l-shape', 'pieces' => ['knight']}
+    ]
+    movement.each do |move|
+      temp = [my_king_pos[0] + move['position'][0], my_king_pos[1] + move['position'][1]]
+      squares = 1
+      if move['continuous'] == true
+        until out_of_bounds?(temp) || my_piece?(temp_board, temp)
+          if squares == 1 && temp_board.any? { |piece| piece.position == temp && piece.type == 'pawn' && piece.color != @color } && move['direction'] == 'diagonal'
+            return true
+          elsif squares == 1 && temp_board.any? { |piece| piece.position == temp && piece.type == 'king' && piece.color != @color } && (move['direction'] == 'diagonal' || move['direction'] == 'straight')
+            return true
+          elsif temp_board.any?{ |piece| piece.position == temp && move['pieces'].include?(piece.type) && piece.color != @color }
+            return true
+          elsif opponent_piece?(temp_board, temp)
+            break
+          else
+            temp = [temp[0] + move['position'][0], temp[1] + move['position'][1]]
+            squares += 1
+          end
         end
+      else
+        if temp_board.any?{ |piece| piece.position == temp && move['pieces'].include?(piece.type) && piece.color != @color }
+          return true
+        end  
       end
     end
     return false
--- spec/chess_utilites_spec.rb
+++ spec/chess_utilites_spec.rb
@@ -1,4 +1,10 @@
 require_relative '../lib/chess_utilities'
+require_relative '../lib/king'
+require_relative '../lib/pawn'
+require_relative '../lib/rook'
+require_relative '../lib/knight'
+require_relative '../lib/bishop'
+require_relative '../lib/queen'
 
 describe ChessUtilites do
   describe '#out_of_bounds' do
@@ -35,135 +41,88 @@ describe ChessUtilites do
   end
 
   describe '#in_check' do
-    class KingDummy
-      include ChessUtilites
-      
-      attr_accessor :color, :position, :type, :possible_moves
+    let(:in_check_king) { King.new('red', [2, 6]) }
+    let(:pawn_check) { Pawn.new('white', [3, 7]) }
+    let(:rook_check) { Rook.new('white', [2, 3]) }
+    let(:knight_check) { Knight.new('white', [4, 5]) }
+    let(:bishop_check) { Bishop.new('white', [1, 7]) }
+    let(:queen_check) { Queen.new('white', [0, 6]) }
+    let(:noncheck_knight) { Knight.new('red', [0, 3]) }
+    let(:noncheck_rook) { Rook.new('white', [3, 5]) }
+    let(:noncheck_bishop) { Bishop.new('red', [7, 2]) }
+    let(:noncheck_queen) { Queen.new('white', [7, 7]) }
+    let(:noncheck_pawn) { Pawn.new('white', [2, 6]) }
+    let(:moving_rook) { Rook.new('red', [2, 5]) }
 
-      def initialize
-        @color = 'red'
-        @position = [2, 6]
-        @type = 'king'
-        @possible_moves = []  
-      end
-    end
-    
-    let(:in_check_king) { KingDummy.new }
-    let(:pawn_check) { double('Pawn', color: 'white', type: 'pawn', position: [3, 7], possible_moves: [[2, 7], [2, 6]]) }
-    let(:rook_check) { double('Rook', color: 'white', type: 'rook', position: [2, 3], possible_moves: [
-      [1, 3],
-      [0, 3],
-      [2, 4],
-      [2, 5],
-      [2, 6]
-    ]) }
-    let(:knight_check) { double('Knight', color: 'white', type: 'knight', position: [4, 5], possible_moves: [
-      [2, 4],
-      [2, 6],
-      [5, 7],
-      [6, 6],
-      [6, 4],
-      [5, 3],
-      [3, 3]
-    ]) }
-    let(:bishop_check) { double('Bishop', color: 'white', type: 'bishop', position: [1, 7], possible_moves: [[2, 6]]) }
-    let(:queen_check) { double('Queen', color: 'white', type: 'queen', position: [0, 6], possible_moves: [[
-      [0, 7],
-      [1, 6],
-      [2, 6],
-      [1, 5],
-      [2, 4],
-      [3, 3],
-      [4, 2],
-      [5, 1],
-      [6, 0]
-    ]]) }
-    let(:noncheck_knight) { double('Knight', color: 'red', type: 'knight', position: [0, 3], possible_moves: [
-      [1, 5],
-      [2, 4],
-      [2, 2],
-      [1, 1]
-      ]) }
-    let(:noncheck_rook) { double('Rook', color: 'white', type: 'rook', position: [2, 5], possible_moves: [
-      [1, 5],
-      [0, 5],
-      [3, 5],
-      [4, 5],
-      [6, 5],
-      [7, 5],
-      [2, 4],
-      [2, 3],
-      [2, 1],
-      [2, 0]
-      ]) }
-    let(:noncheck_bishop) { double('Bishop', color: 'red', type: 'bishop', position: [7, 2], possible_moves: [
-      [6, 1],
-      [5, 0],
-      [6, 3],
-      [5, 4],
-      [4, 5]
-      ]) }
-    let(:noncheck_queen) { double('Queen', color: 'white', type: 'queen', position: [7, 7], possible_moves: [
-      [6, 6],
-      [5, 5],
-      [4, 4],
-      [3, 3],
-      [2, 2],
-      [1, 1],
-      [0, 0],
-      [6, 7],
-      [5, 7],
-      [4, 7],
-      [3, 7],
-      [2, 7],
-      [1, 7],
-      [0, 7],
-      [7, 6],
-      [7 ,5],
-      [7, 4],
-      [7, 3],
-      [7, 2]
-    ]) }
-
-    let(:board_no_check) {[
+    let(:board_no_check) { [
       in_check_king,
       noncheck_knight,
       noncheck_rook,
       noncheck_bishop,
       noncheck_queen
-    ]}
-    let(:board_one_check) {[
+    ] }
+    let(:board_one_check) { [
       in_check_king,
       knight_check,
       noncheck_knight,
       noncheck_rook,
       noncheck_bishop,
       noncheck_queen
-    ]}
-    let(:board_multi_check) {[
+    ] }
+    let(:board_multi_check) { [
       in_check_king,
       pawn_check,
       rook_check,
       bishop_check,
       knight_check,
       queen_check
-    ]}
+    ] }
+    let(:board_capture_check) { [
+      in_check_king,
+      noncheck_pawn,
+      knight_check
+    ] }
+    let(:board_moved_check) { [
+      in_check_king,
+      rook_check,
+      moving_rook
+    ] }
 
     context 'when the King is not in check' do
       it 'returns false' do
+        board_no_check.each { |piece| piece.update_possible_moves(board_no_check) }
         expect(in_check_king.in_check?(board_no_check, [2, 6])).to eq(false)
       end
     end
 
     context 'when the King is in check by one chess piece' do
       it 'returns true' do
+        board_one_check.each { |piece| piece.update_possible_moves(board_one_check) }
         expect(in_check_king.in_check?(board_one_check, [2, 6])).to eq(true)
       end
     end
 
     context 'when the King is in check by multiple pieces' do
       it 'returns true' do
-        expect(in_check_king.in_check?(board_multi_check, [2, 6]))
+        board_multi_check.each { |piece| piece.update_possible_moves(board_multi_check) }
+        expect(in_check_king.in_check?(board_multi_check, [2, 6])).to eq(true)
+      end
+    end
+
+    context 'when the King captures a piece that puts itself in check' do
+      it 'returns true' do
+        in_check_king.update_position([1, 6])
+        board_capture_check.each { |piece| piece.update_possible_moves(board_capture_check) }
+        expect(in_check_king.in_check?(board_capture_check, [1, 6])).to eq(false)
+        expect(in_check_king.in_check?(board_capture_check, [2, 6])).to eq(true)
+      end
+    end
+
+    context 'when a piece tries to move but it would its King in check' do
+      it 'returns true' do
+        board_moved_check.each { |piece| piece.update_possible_moves(board_moved_check) }
+        expect(moving_rook.in_check?(board_moved_check, [2, 5])).to eq(false)
+        expect(moving_rook.in_check?(board_moved_check, [1, 5])).to eq(true)
       end
     end
   end
